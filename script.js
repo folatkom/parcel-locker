f383072996ecae6e70618e95fc5fb057bbebeb66
Monika Folatko <folatko.monika@gmail.com>
2022-02-03 23:28:03 +0100

    Create basic functionality

---
+++ script.js
@@ -0,0 +1,60 @@
+const getParcel = document.querySelector('.getParcel');
+const inputs = document.getElementById('inputs');
+const alert = document.getElementById('alert');
+const success = document.getElementById('success');
+const nextParcel = document.getElementById('nextParcel');
+let phone;
+let code;
+let isClicked = false;
+const regexCode = /^\d{4}$/;
+const regexPhone = /^(\d{9}|\d{3}[-\s]\d{2}[-\s]\d{2}[-\s]\d{2})$/;
+
+const getNextParcel = () => {
+    document.getElementById('phone').value = '';
+    document.getElementById('code').value = '';
+    inputs.classList.remove('invisible');
+    inputs.classList.add('visible');
+    getParcel.classList.remove('invisible');
+    success.classList.add('invisible'); 
+    getParcel.addEventListener('click', showInputs);
+}
+
+const validate = () => {
+    if(regexPhone.test(phone) && regexCode.test(code)){
+        inputs.classList.add('invisible');
+        inputs.classList.remove('visible');
+        success.classList.remove('invisible'); 
+        getParcel.classList.add('invisible');
+        alert.innerHTML = '';
+        nextParcel.addEventListener('click', getNextParcel);
+    }
+    else{
+        if(regexPhone.test(phone) == false){
+            if(regexCode.test(code)){
+                alert.innerHTML = 'Podano nieprawidłowy numer telefonu.'
+            }
+            else{
+                alert.innerHTML = 'Podano nieprawidłowy numer telefonu i kod odbioru.'
+            }
+        } 
+        else{
+            alert.innerHTML = 'Podano nieprawidłowy kod odbioru.'
+        }
+        getParcel.addEventListener('click', validate);       
+    }
+}
+
+const showInputs = () => {
+    if(isClicked == false){
+        inputs.classList.remove('invisible');
+        inputs.classList.add('visible');
+        isClicked = true;
+    }
+    else {
+        phone = document.getElementById('phone').value;
+        code = document.getElementById('code').value;
+        validate(); 
+    }
+}
+
+getParcel.addEventListener('click', showInputs);
